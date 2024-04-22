f16b8965c31ef0efeb27712a1f9d70dfd5e62d77
GR <96717642+nagiri1084@users.noreply.github.com>
2024-04-22 16:33:14 +0900

    Add controller Move
    Add controller Move

---
+++ MRTKPhotonProjectMR/Assets/Scripts/cshController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshController : MonoBehaviour
+{
+    public int speedForward = 12; //전진 속도
+    public int speedSide = 6; //옆걸음 속도
+
+    private Transform tr; //플레이어 트랜스폼
+    private float dirX = 0;
+    private float dirZ = 0;
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    //플레이어 이동
+    void MovePlayer()
+    {
+        dirX = 0;   //좌우 이동 방향(왼쪽: -1, 오른쪽: 1)
+        dirZ = 0;   //앞뒤 이동 방향(후진: -1, 전진: 1)
+
+        if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
+        {
+            Vector2 coord = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+            var absX = Mathf.Abs(coord.x);
+            var absY = Mathf.Abs(coord.y);
+
+            if(absX > absY)
+            {
+                //right
+                if (coord.x > 0) dirX = +1;
+                //left
+                else dirX = -1;
+            }
+            else
+            {
+                //up
+                if (coord.y > 0) dirZ = +1;
+                //down
+                else dirZ = -1;
+            }
+        }
+        //이동 방향 설정 후 이동
+        Vector3 moveDir = new Vector3(dirX * speedSide, 0, dirZ * speedForward);
+        transform.Translate(moveDir * Time.smoothDeltaTime);
+    }
+}
